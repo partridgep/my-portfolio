895123466ffabb42c2b72b0cb7ca387f03508246
partridgep <paul.partridg@gmail.com>
2022-03-13 16:19:53 -0400

    updates resume and About Me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -24,9 +24,9 @@ export default function About({portrait}) {
                 <div className={styles.text} >
                     <h1>About Me</h1>
                     <p>Hello there, I'm Paul Partridge, and I love the process of assembling pieces of a larger puzzle for others to interact with.</p>
-                    <p>I first put that passion into practice within the documentary film industry, but after a couple of years, I felt the need to scratch the itch I had ever since I took an <i>Intro to Programming</i> course in college. I dedicated myself to learning software engineering, and enrolled in General Assembly's six-month immersive class.</p>
-                    <p>If you would've seen me then, I would have been furiously typing on my keyboard, looking up how to add more and more complexities to my projects. Since then, I have been fortunate to join the team at Peapod Digital Labs, and keep learning by doing, regularly contributing enhancements to a widely-used platform.</p>
-                    <p>When I'm not pushing new code to GitHub, you can find me taking long walks and desperately trying to turn my dog into an influencer on Instagram <a href="https://www.instagram.com/worldofmeatball/" target="_blank" rel="noreferrer">@worldofmeatball</a>.</p>
+                    <p>I first put that passion into practice within the documentary film industry. At the start of the pandemic, I dedicated myself to learning software engineering, and enrolled in General Assembly's six-month immersive class.</p>
+                    <p>If you would've seen me then, I would have been furiously typing on my keyboard, looking up how to add more and more complexities to my projects. Since then, I have worked for both large and small teams at Peapod Digital Labs and Motor City Wash Works, eager to keep learning by doing, regularly contributing enhancements to in-demand platforms.</p>
+                    <p>When I'm not pushing new code to GitHub, you can find me taking long walks and desperately trying to turn my two corgis into influencer dogs on Instagram <a href="https://www.instagram.com/worldofmeatball/" target="_blank" rel="noreferrer">@worldofmeatball</a>.</p>
                 </div>
               </div>
               <div style={{display: 'flex', alignItems: 'center', flexShrink: '0'}}>
